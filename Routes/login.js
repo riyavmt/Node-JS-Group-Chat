e6f80ea6b6e05cd249f5076df1405934ee2a91aa
riyavmt <riyavmt14@gmail.com>
2023-11-10 01:58:26 +0530

    Task done

--- Routes/login.js
+++ Routes/login.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/login',(req,res,next)=>{
-    res.send('<form action = "/login" method="POST" onSubmit="localStorage.setItem("Username", document.getElementById("name").value)"><label for = "name">Username</label><input type = "text" id = "name" name="name"><button type="submit">LOGIN</button></form>')
+    res.send(`<html><head><title>Enter</title></head><body><form action = "/login" method="POST" onSubmit="localStorage.setItem('Username', document.getElementById('name').value)"><label for = "name">Username</label><input type = "text" id = "name" name="name"><button type="submit">LOGIN</button></form></body></html>`)
 });
 router.post('/login',(req,res,next)=>{
     console.log(req.body);
--- Routes/msg.js
+++ Routes/msg.js
@@ -1,12 +1,29 @@
 const express = require('express');
 
 const router = express.Router();
+const fs = require('fs');
 
 router.get("/",(req,res,next)=>{
-    res.send('<form action = "/" method="POST"><label for = "msg">Message</label><input type = "text" id = "msg" name="msg"><button type="submit">Send</button></form>')
+    fs.readFile('message.txt',(err,data)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            const message = data;
+            res.send(`<html><head><title>Enter</title></head><body><form action = "/" method="POST" onSubmit ="document.getElementById('name').value=localStorage.getItem('Username')" ><label for = "msg">Message</label><input type = "text" id = "msg" name="msg"><input type="hidden" id="name" name="name"><button type="submit">Send</button></form><div><pre>${message}</pre></div></body></html>`)
+        }
+    })
+    
 })
 router.post('/',(req,res,next)=>{
-    console.log(req.body);
+    //console.log(req.body);
+    const msg = req.body.name + ": " +req.body.msg;
+
+    fs.appendFile('message.txt',msg + '\n', (err)=>{
+        if(err){
+            console.log(err.msg);
+        }
+    });
     res.redirect('/');
 })
 
